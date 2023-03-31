cd6070b258b471fabf536cba7864382245b3c8c1
widiart <widiartoputra@gmail.com>
2023-03-31 09:30:46 +0700

    update route admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AdminController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -12,14 +12,14 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-// ADMIN
+// USER
 Route::middleware(['auth', 'user-access:user'])->group(function () {
-  
+    
     Route::get('/home', [HomeController::class, 'index'])->name('home');
 });
 
-// USER
+// ADMIN
 Route::middleware(['auth', 'user-access:admin'])->group(function () {
   
-    Route::get('/admin/home', [HomeController::class, 'index'])->name('home');
+    Route::get('/admin/home', [AdminController::class, 'index'])->name('dashboard');
 });
